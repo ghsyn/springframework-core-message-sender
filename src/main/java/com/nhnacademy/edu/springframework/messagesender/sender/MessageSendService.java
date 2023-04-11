d504d7b32f2ae56f8120695d489ac29581a504ea
김시연 <swanshawn715@gmail.com>
2023-04-11 14:30:05 +0900

    DI annotation

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/sender/MessageSendService.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/sender/MessageSendService.java
@@ -5,10 +5,7 @@ import com.nhnacademy.edu.springframework.messagesender.User;
 public class MessageSendService {
     private MessageSender messageSender;
 
-    public MessageSendService() {
-    }
-
-    public void setMessageSender(MessageSender messageSender) {
+    public MessageSendService(@SMS MessageSender messageSender) {
         System.out.println("setMessageSender invoked!");
         this.messageSender = messageSender;
     }
